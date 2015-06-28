d897112393c4baaedefab029c486e3dea9909167
konpyu <konpyu@gmail.com>
2015-06-28 11:36:32 +0900

    photo appearance tweak

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,5 @@
 class TopController < ApplicationController
   def index
-    @photos = Photo.includes(:image, :ward, :town).all
+    @photos = Photo.includes(:image, :ward, :town).order(id: :desc).limit(10)
   end
 end
