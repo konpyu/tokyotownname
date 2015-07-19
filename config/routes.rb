c2b7e618f3c9a60887b86ec6ee82517f5a64ba2b
konpyu <konpyu@gmail.com>
2015-07-19 16:47:01 +0900

    pages added

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   post '/auth/:provider/callback', to: 'sessions#create'
 
+  get 'achieve', to: 'pages#achieve'
+  get 'about',   to: 'pages#about'
+
   resources :lives, only: [:create, :destroy]
   resources :photos
   resources :towns, only: [:show, :index]
