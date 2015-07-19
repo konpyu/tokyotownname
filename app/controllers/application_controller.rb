0a148722668c10739d9f76ca0825cfdedb84f66c
konpyu <konpyu@gmail.com>
2015-07-19 11:42:27 +0900

    lived button improved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :exception
 
   class Tokyotownname::BadRequest < StandardError; end    # 400
   class Tokyotownname::UnAuthorized < StandardError; end  # 401
--- app/controllers/lives_controller.rb
+++ app/controllers/lives_controller.rb
@@ -1,6 +1,13 @@
 class LivesController < ApplicationController
   def create
-    render json: { status: 'error'}
+    livable = params[:livable_type].classify.constantize.find_by(id: params[:livable_id])
+    live = Live.first_or_initialize(user: current_user, livable: livable, kind: params[:kind])
+    if live.new_record?
+      live.save!
+      head :created
+    else
+      render json: { status: 'error' }
+    end
   end
   def destroy
   end
