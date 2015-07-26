36ea941dd153fb374f14bcbca4127f506dba8b66
konpyu <konpyu@gmail.com>
2015-07-26 13:16:57 +0900

    achieve temporary

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,18 @@ class PagesController < ApplicationController
   end
 
   def achieve
+    # num = {}
+    # arrs = Photo.pluck(:ward_id, :town_id)
+    # arrs.each do |arr|
+    #   ward_id = arr[0]
+    #   town_id = arr[1]
+    #
+    #   num[ward_id] ||= []
+    #   num[ward_id] << town_id
+    # end
+    # num.keys.each do |key|
+    #   num[key] = num[key].uniq.length
+    # end
+    @wards = Ward.all
   end
 end
