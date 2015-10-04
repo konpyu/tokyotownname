868b7be5a3b8095f410d287fe63e666d036a93b0
konpyu <konpyu@gmail.com>
2015-10-04 13:44:15 +0900

    tweak

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,18 +10,12 @@ class PagesController < ApplicationController
   end
 
   def achieve
-    # num = {}
-    # arrs = Photo.pluck(:ward_id, :town_id)
-    # arrs.each do |arr|
-    #   ward_id = arr[0]
-    #   town_id = arr[1]
-    #
-    #   num[ward_id] ||= []
-    #   num[ward_id] << town_id
-    # end
-    # num.keys.each do |key|
-    #   num[key] = num[key].uniq.length
-    # end
     @wards = Ward.all
+    gon.archive = {}
+    @wards.each do |ward|
+      gon.archive[ward.id] ||= {}
+      gon.archive[ward.id][:value] = Photo.where(ward_id: ward.id).count # NOTE: has bug
+      gon.archive[ward.id][:max]   = Town.where(ward_id: ward.id).count
+    end
   end
 end
