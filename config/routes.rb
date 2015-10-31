69cbbb4e2cb3391981a6cb801516570ef11a2841
konpyu <konpyu@gmail.com>
2015-10-31 13:35:46 +0900

    add logout link

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'top#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   post '/auth/:provider/callback', to: 'sessions#create'
+  get 'logout',  to: 'sessions#destroy'
 
   get 'achieve', to: 'pages#achieve'
   get 'about',   to: 'pages#about'
